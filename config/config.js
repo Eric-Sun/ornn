28c9c34e17f7264026bd9f67332549300c15646a
sunbo <docsun.bo@gmail.com>
2020-01-16 22:21:33 +0800

    add

---
+++ config/config.js
@@ -0,0 +1,9 @@
+
+let config = {
+    // env: Env,
+    // apiDomain:'http://staffs.api.mingyi.loc',
+    apiDomain:'/api',
+    // apiDomain: 'http://localhost:8088',
+    imgUpload: 'http://localhost:8082/upload'
+};
+export default config;
