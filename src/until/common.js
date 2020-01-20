f0ef00255b73b68068144e8b210530f2fc09425d
sunbo <docsun.bo@gmail.com>
2020-01-20 21:16:49 +0800

    add

--- src/until/common.js
+++ src/until/common.js
@@ -262,3 +262,22 @@ export function checkAuth(auth) {
     return q.indexOf(rule) != -1;
 }
 
+
+export function getTime(data){
+    var _data = data;
+    //如果是13位正常，如果是10位则需要转化为毫秒
+    if (String(data).length == 13) {
+      _data = data
+    } else {
+      _data = data*1000
+    }
+    const time = new Date(_data);    
+    const Y = time.getFullYear();
+    const Mon = time.getMonth() + 1;
+    const Day = time.getDate();
+    const H = time.getHours();
+    const Min = time.getMinutes();
+    const S = time.getSeconds();
+    //自定义选择想要返回的类型
+      return `${Y}-${Mon}-${Day} ${H}:${Min}:${S}`
+  }
